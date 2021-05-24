915384a637e143a1740839a0887fdeb903c95908
vitor mouzinho <vimouzin@MacBook-Pro.lan>
2021-05-24 00:06:18 -0700

    test

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from selenium import webdriver
 
 def test_on_click_output():
 
-    #workDir = os.path.dirname(os.path.realpath(__file__))
-    #path_to_driver = workDir+"/"+"chromedriver"
-    #driver = webdriver.Chrome(path_to_driver);
-    #driver.get(workDir+"/html/index.html");
-    #button = driver.find_element_by_id("button")
-    #button.click();
-    #text = driver.find_element_by_id("demo")
+    workDir = os.path.dirname(os.path.realpath(__file__))
+    path_to_driver = workDir+"/"+"chromedriver"
+    driver = webdriver.Chrome(path_to_driver);
+    driver.get(workDir+"/html/index.html");
+    button = driver.find_element_by_id("button")
+    button.click();
+    text = driver.find_element_by_id("demo")
     return "Hi"
 
 
