81d59ec33513401dfe4214a01ab6e7315e4bb87f
vitor mouzinho <vimouzin@MacBook-Pro.lan>
2021-05-24 00:40:48 -0700

    test

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def test_on_click_output():
     workDir = os.path.dirname(os.path.realpath(__file__))
     #print(workDir)
     path_to_driver = workDir+"/"+"chromedriver"
-    driver = webdriver.Chrome(path_to_driver);
+    driver = webdriver.Chrome("bitot");
     #driver.get(workDir+"/html/index.html");
     #button = driver.find_element_by_id("button")
    # button.click();
