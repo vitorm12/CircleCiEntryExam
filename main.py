598b6424585e9d3b2a5f12460c4f69f12a5d8649
vitor mouzinho <vimouzin@MacBook-Pro.lan>
2021-05-23 23:43:25 -0700

    test

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
+import os
 
+from selenium import webdriver;
 
 
 def test_on_click_output():
-    return "hi"
-#    workDir = os.path.dirname(os.path.realpath(__file__))
- #   path_to_driver = workDir+"/"+"chromedriver"
-  #  driver = webdriver.Chrome(path_to_driver);
-   # driver.get(workDir+"/html/index.html");
-    #button = driver.find_element_by_id("button")
-    #button.click();
-    #text = driver.find_element_by_id("demo")
-    #return text
+    workDir = os.path.dirname(os.path.realpath(__file__))
+    path_to_driver = workDir+"/"+"chromedriver"
+    driver = webdriver.Chrome(path_to_driver);
+    driver.get(workDir+"/html/index.html");
+    button = driver.find_element_by_id("button")
+    button.click();
+    text = driver.find_element_by_id("demo")
+    return text
 
 
 if __name__ == '__main__':
