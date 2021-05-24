9a71188c7ab5684b55d541456e31e9f0012ed59d
vitor mouzinho <vimouzin@MacBook-Pro.lan>
2021-05-24 01:13:06 -0700

    test

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def test_on_click_output():
     workDir = os.path.dirname(os.path.realpath(__file__))
     #print(workDir)
     path_to_driver = workDir+"/"+"chromedriver"
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome('./chromedriver')
     #driver.get(workDir+"/html/index.html");
     #button = driver.find_element_by_id("button")
    # button.click();
