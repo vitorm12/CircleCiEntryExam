e3014a7648aa39e91b2950005b4e681d556477e6
vitor mouzinho <vimouzin@MacBook-Pro.lan>
2021-05-24 00:37:35 -0700

    test

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ def test_on_click_output():
     workDir = os.path.dirname(os.path.realpath(__file__))
     #print(workDir)
     path_to_driver = "vitor"
-    driver = webdriver.Chrome(path_to_driver);
+    driver = webdriver.Firefox()
+    driver.get("https://www.google.com")
     #driver.get(workDir+"/html/index.html");
     #button = driver.find_element_by_id("button")
    # button.click();
     #text = driver.find_element_by_id("demo")
-    return workDir
+    return "Hi"
 
 
 if __name__ == '__main__':
