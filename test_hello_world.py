693b5a9cc86ad63ed420217ff7cca55650be1627
vitor mouzinho <vimouzin@MacBook-Pro.lan>
2021-05-25 22:40:18 -0700

    test

--- test_hello_world.py
+++ test_hello_world.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.keys import Keys
 
 import main
 import unittest
-
+import WebDriver
 
 class TestHelloWorld(unittest.TestCase):
 
@@ -19,19 +19,8 @@ class TestHelloWorld(unittest.TestCase):
     def test_status_code(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        workDir = os.path.dirname(os.path.realpath(__file__))
-        path_to_driver = workDir + "/" + "chromedriver"
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_prefs = {}
-        chrome_options.experimental_options["prefs"] = chrome_prefs
-        chrome_prefs["profile.default_content_settings"] = {"images": 2}
-        driver = webdriver.Chrome(options=chrome_options)
-        #driver.get(workDir+"/html/index.html");
-        #button = driver.find_element_by_id("button")
-        #button.click();
+        with WebDriver() as driver:
+            driver.login()
 
     def test_greeting_message(self):
         greeting = 'Welcome to CI/CD'
