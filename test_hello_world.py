71215a5ba5853a2213c641463e8548e3e1e85867
vitor mouzinho <vimouzin@MacBook-Pro.lan>
2021-05-25 21:55:38 -0700

    test

--- test_hello_world.py
+++ test_hello_world.py
@@ -1,6 +1,7 @@
 import os
 
-from selenium.webdriver.chrome import webdriver
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 import main
 import unittest
@@ -18,9 +19,9 @@ class TestHelloWorld(unittest.TestCase):
         workDir = os.path.dirname(os.path.realpath(__file__))
         path_to_driver = workDir + "/" + "chromedriver"
         driver = webdriver.Chrome(path_to_driver)
-        driver.get(workDir+"/html/index.html");
-        button = driver.find_element_by_id("button")
-        button.click();
+        #driver.get(workDir+"/html/index.html");
+        #button = driver.find_element_by_id("button")
+        #button.click();
 
     def test_greeting_message(self):
         greeting = 'Welcome to CI/CD'
